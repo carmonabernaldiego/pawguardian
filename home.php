9b612f346032a103a44878147c5aaaa90c540d6c
Diego Carmona Bernal <cbdiegox@gmail.com>
2023-11-21 03:06:26 -0600

    update

--- home.php
+++ home.php
@@ -71,7 +71,7 @@ include_once 'modules/notif_info_unset.php';
           </div>
         </div>
 
-        <div class="row">
+        <!--<div class="row">
           <div class="col-12 col-xl-12 stretch-card">
             <div class="row flex-grow">
               <div class="col-md-4 grid-margin stretch-card">
@@ -199,9 +199,10 @@ include_once 'modules/notif_info_unset.php';
               </div>
             </div>
           </div>
-        </div> <!-- row -->
+        </div>-->
+        <!-- row -->
 
-        <div class="row">
+        <!--<div class="row">
           <div class="col-12 col-xl-12 grid-margin stretch-card">
             <div class="card overflow-hidden">
               <div class="card-body">
@@ -246,37 +247,19 @@ include_once 'modules/notif_info_unset.php';
               </div>
             </div>
           </div>
-        </div> <!-- row -->
+        </div> -->
+        <!-- row -->
 
         <div class="row">
           <div class="col-lg-7 col-xl-8 grid-margin stretch-card">
             <div class="card">
               <div class="card-body">
-                <div class="d-flex justify-content-between align-items-baseline mb-2">
-                  <h6 class="card-title mb-0">Monthly sales</h6>
-                  <div class="dropdown mb-2">
-                    <button class="btn p-0" type="button" id="dropdownMenuButton4" data-toggle="dropdown"
-                      aria-haspopup="true" aria-expanded="false">
-                      <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton4">
-                      <a class="dropdown-item d-flex align-items-center" href="#"><i data-feather="eye"
-                          class="icon-sm mr-2"></i> <span class="">View</span></a>
-                      <a class="dropdown-item d-flex align-items-center" href="#"><i data-feather="edit-2"
-                          class="icon-sm mr-2"></i> <span class="">Edit</span></a>
-                      <a class="dropdown-item d-flex align-items-center" href="#"><i data-feather="trash"
-                          class="icon-sm mr-2"></i> <span class="">Delete</span></a>
-                      <a class="dropdown-item d-flex align-items-center" href="#"><i data-feather="printer"
-                          class="icon-sm mr-2"></i> <span class="">Print</span></a>
-                      <a class="dropdown-item d-flex align-items-center" href="#"><i data-feather="download"
-                          class="icon-sm mr-2"></i> <span class="">Download</span></a>
-                    </div>
-                  </div>
+                <div class="d-flex justify-content-between align-items-baseline mb-4">
+                  <h6 class="card-title mb-0">Temperatura °C</h6>
                 </div>
-                <p class="text-muted mb-4">Sales are activities related to selling or the number of goods or services
-                  sold in a given time period.</p>
+                <br />
                 <div class="monthly-sales-chart-wrapper">
-                  <canvas id="monthly-sales-chart"></canvas>
+                  <canvas id="temperature-chart"></canvas>
                 </div>
               </div>
             </div>
@@ -284,52 +267,16 @@ include_once 'modules/notif_info_unset.php';
           <div class="col-lg-5 col-xl-4 grid-margin stretch-card">
             <div class="card">
               <div class="card-body">
-                <div class="d-flex justify-content-between align-items-baseline mb-2">
-                  <h6 class="card-title mb-0">Cloud storage</h6>
-                  <div class="dropdown mb-2">
-                    <button class="btn p-0" type="button" id="dropdownMenuButton5" data-toggle="dropdown"
-                      aria-haspopup="true" aria-expanded="false">
-                      <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton5">
-                      <a class="dropdown-item d-flex align-items-center" href="#"><i data-feather="eye"
-                          class="icon-sm mr-2"></i> <span class="">View</span></a>
-                      <a class="dropdown-item d-flex align-items-center" href="#"><i data-feather="edit-2"
-                          class="icon-sm mr-2"></i> <span class="">Edit</span></a>
-                      <a class="dropdown-item d-flex align-items-center" href="#"><i data-feather="trash"
-                          class="icon-sm mr-2"></i> <span class="">Delete</span></a>
-                      <a class="dropdown-item d-flex align-items-center" href="#"><i data-feather="printer"
-                          class="icon-sm mr-2"></i> <span class="">Print</span></a>
-                      <a class="dropdown-item d-flex align-items-center" href="#"><i data-feather="download"
-                          class="icon-sm mr-2"></i> <span class="">Download</span></a>
-                    </div>
-                  </div>
+                <div class="d-flex justify-content-between align-items-baseline mb-4">
+                  <h6 class="card-title mb-0">Humedad</h6>
                 </div>
-                <div id="progressbar1" class="mx-auto"></div>
-                <div class="row mt-4 mb-3">
-                  <div class="col-6 d-flex justify-content-end">
-                    <div>
-                      <label
-                        class="d-flex align-items-center justify-content-end tx-10 text-uppercase font-weight-medium">Total
-                        storage <span class="p-1 ml-1 rounded-circle bg-primary-muted"></span></label>
-                      <h5 class="font-weight-bold mb-0 text-right">8TB</h5>
-                    </div>
-                  </div>
-                  <div class="col-6">
-                    <div>
-                      <label class="d-flex align-items-center tx-10 text-uppercase font-weight-medium"><span
-                          class="p-1 mr-1 rounded-circle bg-primary"></span> Used storage</label>
-                      <h5 class="font-weight-bold mb-0">6TB</h5>
-                    </div>
-                  </div>
-                </div>
-                <button class="btn btn-primary btn-block">Upgrade storage</button>
+                <br />
+                <div id="humidityChart" class="mx-auto"></div>
               </div>
             </div>
           </div>
-        </div> <!-- row -->
-
-
+        </div>
+        <!-- row -->
 
       </div>
 
@@ -359,6 +306,7 @@ include_once 'modules/notif_info_unset.php';
   <script src="../assets/js/template.js"></script>
   <!-- endinject -->
   <!-- custom js for this page -->
+  <script src="/js/firebase.js" type="module"></script>
   <script src="../assets/js/dashboard.js"></script>
   <script src="../assets/js/datepicker.js"></script>
   <!-- end custom js for this page -->
---
+++ js/firebase.js
@@ -0,0 +1,157 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
+
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD4mLUtCZWOmd9pd_J6cQoFwSguSL8dMNY",
+  authDomain: "paw-guardian.firebaseapp.com",
+  databaseURL: "https://paw-guardian-default-rtdb.firebaseio.com",
+  projectId: "paw-guardian",
+  storageBucket: "paw-guardian.appspot.com",
+  messagingSenderId: "430138885292",
+  appId: "1:430138885292:web:86a92a2fdfde91226f8b09",
+  measurementId: "G-X12BZP96NW",
+};
+
+// Iniciar Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const dbRef = ref(db, "TemperatureSensor");
+
+var gridLineColor = "rgba(77, 138, 240, .1)";
+
+var colors = {
+  primary: "#727cf5",
+  secondary: "#7987a1",
+  success: "#42b72a",
+  info: "#68afff",
+  warning: "#fbbc06",
+  danger: "#ff3366",
+  light: "#ececec",
+  dark: "#282f3a",
+  muted: "#686868",
+};
+
+// Función para formatear la fecha
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+}
+
+// Referencia a los sensores en Firebase
+const temperatureRef = ref(db, "TemperatureSensor");
+const humidityRef = ref(db, "HumiditySensor");
+
+// Crear el gráfico de temperatura
+let myChart;
+const temperatureChart = document
+  .getElementById("temperature-chart")
+  .getContext("2d");
+
+// Crear el círculo de progreso para la humedad
+let humidityBar;
+if (document.getElementById("humidityChart")) {
+  humidityBar = new ProgressBar.Circle(humidityChart, {
+    color: colors.primary,
+    trailColor: gridLineColor,
+    strokeWidth: 4,
+    trailWidth: 1,
+    easing: "easeInOut",
+    duration: 1400,
+    text: {
+      autoStyleContainer: false,
+    },
+    from: { color: colors.primary, width: 1 },
+    to: { color: colors.primary, width: 4 },
+    step: function (state, circle) {
+      circle.path.setAttribute("stroke", state.color);
+      circle.path.setAttribute("stroke-width", state.width);
+      var value = Math.round(circle.value() * 100);
+      if (value === 0) {
+        circle.setText("");
+      } else {
+        circle.setText(value + "%");
+      }
+    },
+  });
+  humidityBar.text.style.fontFamily = "'Overpass', sans-serif";
+  humidityBar.text.style.fontSize = "3rem";
+}
+
+// Escucha cambios en los datos de temperatura
+onValue(temperatureRef, (snapshot) => {
+  const data = snapshot.val();
+  const temperaturaData = [];
+  if (data) {
+    Object.entries(data).forEach(([key, entry]) => {
+      temperaturaData.push({
+        fecha: formatDate(entry.Date),
+        temperatura: entry.Value,
+      });
+    });
+    if (!myChart) {
+      myChart = new Chart(temperatureChart, {
+        type: "line",
+        data: {
+          labels: temperaturaData.map((d) => d.fecha),
+          datasets: [
+            {
+              label: "Temperatura",
+              data: temperaturaData.map((d) => d.temperatura),
+              backgroundColor: colors.primary,
+              fill: false,
+            },
+          ],
+        },
+        options: {
+          maintainAspectRatio: false,
+          legend: {
+            display: false,
+          },
+          scales: {
+            xAxes: [
+              {
+                ticks: {
+                  fontColor: "#8392a5",
+                  fontSize: 10,
+                },
+              },
+            ],
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                  fontColor: "#8392a5",
+                  fontSize: 10,
+                },
+              },
+            ],
+          },
+        },
+      });
+    } else {
+      myChart.data.labels = temperaturaData.map((d) => d.fecha);
+      myChart.data.datasets.forEach((dataset) => {
+        dataset.data = temperaturaData.map((d) => d.temperatura);
+      });
+      myChart.update();
+    }
+  }
+});
+
+// Escucha cambios en los datos de humedad
+onValue(humidityRef, (snapshot) => {
+  const data = snapshot.val();
+  if (data) {
+    const lastEntry = Object.entries(data).pop();
+    if (lastEntry) {
+      const humidityValue = lastEntry[1].Value;
+      humidityBar.animate(humidityValue / 100); // Asume que Value es un porcentaje
+    }
+  }
+});
